b1cd59781bf4cd47b70233fabeffdd8e44b9afd3
uratomo <rop.ibuki@gmail.com>
2019-07-31 02:08:35 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import firebase from "firebase"
 
 Vue.config.productionTip = false
+
+
+
+  {/* // Your web app's Firebase configuration */}
+  const firebaseConfig = {
+    apiKey: "AIzaSyBvCmmynnbyayJHPmXEzlQ8TsoJDYJ1NHA",
+    authDomain: "my-address-pj-3ed33.firebaseapp.com",
+    databaseURL: "https://my-address-pj-3ed33.firebaseio.com",
+    projectId: "my-address-pj-3ed33",
+    storageBucket: "",
+    messagingSenderId: "10204680750",
+    appId: "1:10204680750:web:0880fd07683244e0"
+  };
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
